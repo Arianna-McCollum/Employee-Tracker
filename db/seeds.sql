5402a428c31df87b1f657d1b1d648041e099bb99
Electrolion <AMccollum71@gmail.com>
2021-09-10 12:26:13 -0400

    Tables created

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO employee
+  (first_name, last_name, role_id, manager_id)
+VALUES
+('James', 'Fraser', 1, 0),
+  ('Jack', 'London', 2, 1),
+  ('Robert', 'Bruce', 3, 0),
+  ('Peter', 'Greenaway', 7, 0),
+  ('Derek', 'Jarman', 8, 1);
