b80f817fae338b10c1c8bfc78abb2d1d71408990
Electrolion <AMccollum71@gmail.com>
2021-09-11 22:42:31 -0400

    Constraint added

--- db/schema.sql
+++ db/schema.sql
@@ -11,7 +11,8 @@ CREATE TABLE role (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
-  department_id INTEGER NOT NULL
+  department_id INTEGER NOT NULL,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE employee (
